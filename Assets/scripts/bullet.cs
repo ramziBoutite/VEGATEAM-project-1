ed1173a5b1e8f79fea59771f4b7c207a2acb0268
Mouad Kacemi <119363581+MouadKacemi@users.noreply.github.com>
2023-07-07 20:42:48 +0100

    tried some conditions but they made bugs

--- Assets/scripts/bullet.cs
+++ Assets/scripts/bullet.cs
@@ -8,28 +8,15 @@ public class bullet : MonoBehaviour
     private Vector3 mousePos;
     private Camera mainCam;
     private Rigidbody2D rb;
+    public mouver dir;
     public float force;
-    public mouver mov2;
+    
     void Start()
     {
          mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
          rb = GetComponent<Rigidbody2D>();
-         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-         Vector3 direction = mousePos - transform.position;
-         
        
-
-
-
-          if (direction.x > 0f)
-        {
-            rb.velocity = new Vector2(direction.x, 0).normalized * force;
-        }
-        else
-        {
-           
-            Destroy(gameObject);
-        }
+        
        }
        
         
@@ -42,6 +29,30 @@ public class bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+          mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+         Vector3 direction = mousePos - transform.position;
+         
+       
+
+
+
+          if (direction.x > 0f && dir.transform.localScale == new Vector3(1,1,1) )
+        {
+            rb.velocity = new Vector2(direction.x, 0).normalized * force;
+        }
+        else
+        {
+           
+            Destroy(gameObject);
+        }
+         if (direction.x < 0f && dir.transform.localScale == new Vector3(-1,1,1) )
+        {
+            rb.velocity = new Vector2(direction.x, 0).normalized * force;
+        }
+        else
+        {
+           
+            Destroy(gameObject);
+        }
     }
 }
