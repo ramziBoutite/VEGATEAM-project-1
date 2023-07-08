9d40fcd1dcc85af1ccb5e191956e72a851a57b28
ramziBoutite <ramzi.b.2003@gmail.com>
2023-07-08 14:23:45 +0200

    added mine with explosion

---
+++ Assets/scripts/mine.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mine : MonoBehaviour
+{
+    private Animator anim;
+    private void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            anim.SetBool("explode", true);
+        }
+        
+    }
+}
--- Assets/scripts/mouver.cs
+++ Assets/scripts/mouver.cs
@@ -46,7 +46,7 @@ public class mouver : MonoBehaviour
         }
 
     }
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnCollisionStay2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("gnd"))
         {
