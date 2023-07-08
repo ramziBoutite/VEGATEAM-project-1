ee8df17bca6adb2ec278b04bed7e8d652795e4ab
Mouad Kacemi <119363581+MouadKacemi@users.noreply.github.com>
2023-07-08 19:29:04 +0100

    removed lockedcursor

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -7,16 +7,16 @@ public class GameManager : MonoBehaviour
 {
     void Start() {
       Cursor.visible = false;
-      Cursor.lockState =CursorLockMode.Locked;
+      
    }
     void Update() {
       if(gameOverUI.activeInHierarchy)
       {
           Cursor.visible = true;
-      Cursor.lockState =CursorLockMode.Locked;
+      
       }else{
           Cursor.visible = false;
-      Cursor.lockState =CursorLockMode.Locked;
+     
       }
    }
    public GameObject gameOverUI;
