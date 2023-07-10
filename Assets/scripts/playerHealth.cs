eb0e9a17a8d8b57d999a2758df346dfc9f513ac3
ramziBoutite <ramzi.b.2003@gmail.com>
2023-07-10 09:55:57 +0200

    underground ...
    b9a selom
    healthbar mton9osch bs7 y9der ymot

--- Assets/scripts/playerHealth.cs
+++ Assets/scripts/playerHealth.cs
@@ -12,52 +12,58 @@ public class playerHealth : MonoBehaviour
     public Image healthBar;
     public GameManager gameManager;
     private bool isDead;
+    private float deadTimer;
+    public float deadSoundTime;
     // Start is called before the first frame update
     void Start()
     {
-        maxhealth=health;
-        
-    }
-    
+        maxhealth = health;
 
-    
+    }
+
+
+
     // Update is called once per frame
     void Update()
     {
         healthBar.fillAmount = Mathf.Clamp(health / maxhealth, 0, 1);
-        if(health <= 0 && !isDead){
+        if (health <= 0 && !isDead)
+        {
 
-            
-            GameObject.FindGameObjectWithTag("enemy").GetComponent<enemyAi>().gameOver = false;
+            FindObjectOfType<AudioManager>().Play("Death");
 
             GameObject.FindGameObjectWithTag("enemy").GetComponent<enemyAi>().gameOver = true;
 
             isDead = true;
-            gameObject.SetActive(false);
-         
-FindObjectOfType<AudioManager>().Play("Death");
-     IEnumerator Start()
-    {
-        yield return StartCoroutine(FirstTask());
-        
-    }
-    
+            if (isDead)
+            {
+                deadTimer -= Time.deltaTime; if (deadTimer > deadSoundTime) { gameObject.SetActive(false); }
 
-     IEnumerator FirstTask()
-    {
-        
-        print("wait");
-        yield return new WaitForSeconds(4f);
-    }
 
-     
-    SceneManager.LoadScene("GameOver");
-}
 
-             
-            }         
-            
+                IEnumerator Start()
+                {
+                    yield return StartCoroutine(FirstTask());
+
+                }
+
+
+                IEnumerator FirstTask()
+                {
+
+                    print("wait");
+                    yield return new WaitForSeconds(4f);
+                }
+
+
+                SceneManager.LoadScene("GameOver");
+            }
+
+
         }
+
+    }
+}
     
     
 
